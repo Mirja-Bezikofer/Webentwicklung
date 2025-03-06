f6a1eee1274e9b87bf4a90b637a095881fbec5da
Mirja-Bezikofer <bezikofer@stud.dhbw-ravensburg.de>
2025-03-06 10:45:13 +0100

    Update side.js

--- Mirja/side.js
+++ Mirja/side.js
@@ -1,9 +1,1 @@
-function openAl(menuID) {
-    var menu = document.getElementById(menuID);
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
 
